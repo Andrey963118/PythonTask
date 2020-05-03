8c929e7e2410e806b58ee5b2da112f4057c546a7
Andrey963118 <64706426+Andrey963118@users.noreply.github.com>
2020-05-03 05:20:48 +0300

    HomeTask2

---
+++ HomeTask2.py
@@ -0,0 +1,4 @@
+def my_func(name, surname, year, city, email, phone):
+    print(name, surname, year, city, email, phone)
+
+my_func(name= 'Andrey', surname='agf', year=1992, city='Spb', email='email', phone='0232')
